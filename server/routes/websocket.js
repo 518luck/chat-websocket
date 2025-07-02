a67be7e8a43d9d256f3067a8cc31c8cb00a34330
蜻蜓ink队长 <165388484+518luck@users.noreply.github.com>
2025-07-02 22:45:43 +0800

    WebSocket端口连接成功

--- server/routes/websocket.js
+++ server/routes/websocket.js
@@ -2,10 +2,10 @@
 //使用第三方库 ws库来创建一个webSocket服务器
 var express = require('express')
 var router = express.Router()
+const websocket = require('ws')
 
-/* GET home page. */
-router.get('/', function (req, res, next) {
-  res.render('index', { title: 'Express' })
-})
+const socket = new websocket.Server({ port: 8080 })
+
+console.log('WebSocket 服务器已启动，端口 8080')
 
 module.exports = router
